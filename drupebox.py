f4339d27e8addc691b8e8127f61f893b9f4cf33c
Duncan <duncan.hawthorne@gmail.com>
2023-05-04 18:31:04 +0100

    formatting standardisation

--- drupebox.py
+++ drupebox.py
@@ -1,33 +1,42 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 from libs_drupe import *
 
 config = get_config()
 
-max_file_size = int(config['max_file_size'])
-dropbox_local_path = config['dropbox_local_path']
+max_file_size = int(config["max_file_size"])
+dropbox_local_path = config["dropbox_local_path"]
 
-db_client = dropbox.Dropbox(app_key = app_key, oauth2_refresh_token = config['refresh_token'])
+db_client = dropbox.Dropbox(
+    app_key=app_key, oauth2_refresh_token=config["refresh_token"]
+)
+
 
 def action_locally_deleted_files():
-    fyi('Actioning locally deleted files')
+    fyi("Actioning locally deleted files")
     file_tree_from_last_run = load_tree()
     file_tree_now = get_tree()
-    locally_deleted_files = determine_deleted_files(file_tree_now,
-            file_tree_from_last_run)
+    locally_deleted_files = determine_deleted_files(
+        file_tree_now, file_tree_from_last_run
+    )
     for locally_deleted_file in locally_deleted_files:
-        info('Found local file deleted, so delete on dropbox '+locally_deleted_file)
+        info("Found local file deleted, so delete on dropbox " + locally_deleted_file)
         try:
-            db_client.files_delete("/"+locally_deleted_file[len(dropbox_local_path):])
-        except :
-            info('Tried to delete a file on dropbox, but it was not there '+locally_deleted_file)
+            db_client.files_delete(
+                "/" + locally_deleted_file[len(dropbox_local_path) :]
+            )
+        except:
+            info(
+                "Tried to delete a file on dropbox, but it was not there "
+                + locally_deleted_file
+            )
     store_tree(file_tree_now)
 
 
 def action_folder(remote_folder_path):
     fyi(remote_folder_path)
     for excluded_path in config["excluded_paths"]:
-        if remote_folder_path[0:len(excluded_path)] == excluded_path:
+        if remote_folder_path[0 : len(excluded_path)] == excluded_path:
             info("Path excluded")
             return
     local_folder_path = dropbox_local_path + remote_folder_path
@@ -36,84 +45,95 @@ def action_folder(remote_folder_path):
     remote_folder_checked_time = time.time()
     for remote_item in remote_folder:
         if time.time() > remote_folder_checked_time + 60:
-            info('last checked in with server over 60 seconds ago, refreshing')
+            info("last checked in with server over 60 seconds ago, refreshing")
             action_folder(remote_folder_path)
             return
         remote_file_path = remote_item.path_display
         local_file_path = dropbox_local_path + remote_file_path[1:]
-        if not path_exists(local_file_path) \
-            or not isinstance(remote_item, dropbox.files.FolderMetadata) \
-            and unix_time(remote_item.client_modified) > local_item_modified_time(local_file_path):# unix_time(remote_item['modified']) > local_item_modified_time(local_file_path):
-            info('Found new file on remote, or remote file has been updated - downloading '
-                  + remote_file_path)
+        if (
+            not path_exists(local_file_path)
+            or not isinstance(remote_item, dropbox.files.FolderMetadata)
+            and unix_time(remote_item.client_modified)
+            > local_item_modified_time(local_file_path)
+        ):  # unix_time(remote_item['modified']) > local_item_modified_time(local_file_path):
+            info(
+                "Found new file on remote, or remote file has been updated - downloading "
+                + remote_file_path
+            )
             if isinstance(remote_item, dropbox.files.FolderMetadata):
                 if not path_exists(local_file_path):
                     os.makedirs(local_file_path)
                 else:
-                    'Modification time on a folder does not matter - no action'
+                    "Modification time on a folder does not matter - no action"
 
             else:
                 download(db_client, remote_file_path, local_file_path)
                 fix_local_time(db_client, remote_file_path)
 
-        elif not isinstance(remote_item, dropbox.files.FolderMetadata) \
-            and local_item_modified_time(local_file_path) > unix_time(remote_item.client_modified):
+        elif not isinstance(
+            remote_item, dropbox.files.FolderMetadata
+        ) and local_item_modified_time(local_file_path) > unix_time(
+            remote_item.client_modified
+        ):
 
             if not isinstance(remote_item, dropbox.files.FolderMetadata):
-                info('Local file has been updated - uploading '
-                     + remote_file_path)
+                info("Local file has been updated - uploading " + remote_file_path)
                 if os.path.getsize(local_file_path) < max_file_size:
                     upload(db_client, local_file_path, remote_file_path)
                     fix_local_time(db_client, remote_file_path)
                 else:
-                    info('File above max size, ignoring: '
-                         + remote_file_path)
+                    info("File above max size, ignoring: " + remote_file_path)
             else:
-                'Modification time on a folder does not matter - no action'
+                "Modification time on a folder does not matter - no action"
 
     for local_item in os.listdir(local_folder_path):
         if time.time() > remote_folder_checked_time + 60:
-            info('last checked in with server over 60 seconds ago, refreshing')
+            info("last checked in with server over 60 seconds ago, refreshing")
             action_folder(remote_folder_path)
             return
-        remote_file_path = '/' + remote_folder_path + local_item
+        remote_file_path = "/" + remote_folder_path + local_item
         local_file_path = local_folder_path + local_item
         if skip(local_file_path):
             continue
-        if local_item_not_found_at_remote(remote_folder,
-                remote_file_path):
+        if local_item_not_found_at_remote(remote_folder, remote_file_path):
             local_time = local_item_modified_time(local_file_path)
-            remote_time_of_deleted_file = False # remote_item_modified_with_deleted(db_client, remote_file_path) #Can't do this test currently, so blanking, and never do local delete following delete on server
-            if False and remote_time_of_deleted_file > local_time: #Can't do this test currently, so blanking, and never do local delete following delete on server
-                info('Unnaccounted file - Modified time for deleted remote file is latest - delete '
-                      + remote_file_path)
+            remote_time_of_deleted_file = False  # remote_item_modified_with_deleted(db_client, remote_file_path) #Can't do this test currently, so blanking, and never do local delete following delete on server
+            if (
+                False and remote_time_of_deleted_file > local_time
+            ):  # Can't do this test currently, so blanking, and never do local delete following delete on server
+                info(
+                    "Unnaccounted file - Modified time for deleted remote file is latest - delete "
+                    + remote_file_path
+                )
                 if os.path.isdir(local_file_path):
                     os.rmdir(local_file_path)
                 else:
                     os.remove(local_file_path)
             else:
-                info('Unnaccounted file - Local is latest to modified - upload '
-                      + remote_file_path)
+                info(
+                    "Unnaccounted file - Local is latest to modified - upload "
+                    + remote_file_path
+                )
                 if os.path.isdir(local_file_path):
-                    info('New file is a folder, dont upload it but just create a folder directly on dropbox - subfiles will get picked up later '
-                          + remote_file_path)
+                    info(
+                        "New file is a folder, dont upload it but just create a folder directly on dropbox - subfiles will get picked up later "
+                        + remote_file_path
+                    )
                     db_client.files_create_folder(remote_file_path)
                 else:
                     if os.path.getsize(local_file_path) < max_file_size:
-                        upload(db_client, local_file_path,
-                               remote_file_path)
+                        upload(db_client, local_file_path, remote_file_path)
                         fix_local_time(db_client, remote_file_path)
                     else:
-                        info('File above max size, ignoring: '
-                             + remote_file_path)
+                        info("File above max size, ignoring: " + remote_file_path)
 
     for sub_folder in os.listdir(local_folder_path):
         if os.path.isdir(local_folder_path + sub_folder):
             if not skip(local_folder_path + sub_folder):
-                action_folder(remote_folder_path + sub_folder + '/')
+                action_folder(remote_folder_path + sub_folder + "/")
 
 
 action_locally_deleted_files()
-fyi('Actioning all other local and remote files changes')
-action_folder('')
-print('Sync complete at ', readable_time(time.time()))
+fyi("Actioning all other local and remote files changes")
+action_folder("")
+print("Sync complete at ", readable_time(time.time()))
--- libs_drupe.py
+++ libs_drupe.py
@@ -1,55 +1,67 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 import os
 import time
 import dropbox
 from datetime import datetime
-tmp_file_location = '/dev/shm/'
+
+tmp_file_location = "/dev/shm/"
 
 # Get your app key from the Dropbox developer website for your app
-app_key = '1skff241na3x0at'
+app_key = "1skff241na3x0at"
+
 
 def fyi(text):
-    print('    ' + text)
+    print("    " + text)
 
 
 def info(text):
-    print('>>> ' + text)
+    print(">>> " + text)
 
 
 def get_config_real():
     from configobj import ConfigObj
-    if not os.path.exists(os.path.join(os.getenv('HOME'), '.config')):
-        os.makedirs(os.path.join(os.getenv('HOME'), '.config'))
-    config_filename = os.path.join(os.getenv('HOME'), '.config',
-                                   'drupebox')
+
+    if not os.path.exists(os.path.join(os.getenv("HOME"), ".config")):
+        os.makedirs(os.path.join(os.getenv("HOME"), ".config"))
+    config_filename = os.path.join(os.getenv("HOME"), ".config", "drupebox")
     if not path_exists(config_filename):
         config = ConfigObj()
         config.filename = config_filename
 
-        flow = dropbox.DropboxOAuth2FlowNoRedirect(app_key,
-                use_pkce=True, token_access_type='offline')
+        flow = dropbox.DropboxOAuth2FlowNoRedirect(
+            app_key, use_pkce=True, token_access_type="offline"
+        )
         authorize_url = flow.start()
-        print(('1. Go to: ' + authorize_url))
+        print(("1. Go to: " + authorize_url))
         print('2. Click "Allow" (you might have to log in first)')
-        print('3. Copy the authorization code.')
-        code = input('Enter the authorization code here: ').strip()
+        print("3. Copy the authorization code.")
+        code = input("Enter the authorization code here: ").strip()
         result = flow.finish(code)
 
-        (config['access_token'], config['user_id'], config['refresh_token']) = result.access_token, result.user_id, result.refresh_token
-        config['dropbox_local_path'] = \
-            input('Enter dropbox local path (or press enter for '
-                      + os.path.join(os.getenv('HOME'), 'Dropbox')
-                      + '/) ').strip()
-        if config['dropbox_local_path'] == '':
-            config['dropbox_local_path'] = os.path.join(os.getenv('HOME'
-                    ), 'Dropbox') + '/'
-        if config['dropbox_local_path'][-1] != "/":
-            config['dropbox_local_path'] = config['dropbox_local_path']+"/"
-        if not path_exists(config['dropbox_local_path']):
-            os.makedirs(config['dropbox_local_path'])    
-        config['max_file_size'] = 10000000
-        config['excluded_paths'] = ["\"/home/pi/SUPER SECRET LOCATION 1\"", "\"/home/pi/SUPERSECRET LOCATION 2\""]
+        (config["access_token"], config["user_id"], config["refresh_token"]) = (
+            result.access_token,
+            result.user_id,
+            result.refresh_token,
+        )
+        config["dropbox_local_path"] = input(
+            "Enter dropbox local path (or press enter for "
+            + os.path.join(os.getenv("HOME"), "Dropbox")
+            + "/) "
+        ).strip()
+        if config["dropbox_local_path"] == "":
+            config["dropbox_local_path"] = (
+                os.path.join(os.getenv("HOME"), "Dropbox") + "/"
+            )
+        if config["dropbox_local_path"][-1] != "/":
+            config["dropbox_local_path"] = config["dropbox_local_path"] + "/"
+        if not path_exists(config["dropbox_local_path"]):
+            os.makedirs(config["dropbox_local_path"])
+        config["max_file_size"] = 10000000
+        config["excluded_paths"] = [
+            '"/home/pi/SUPER SECRET LOCATION 1"',
+            '"/home/pi/SUPERSECRET LOCATION 2"',
+        ]
         config.write()
 
     config = ConfigObj(config_filename)
@@ -57,50 +69,53 @@ def get_config_real():
 
 
 def get_config():
-    if get_config.cache == '': #First run
+    if get_config.cache == "":  # First run
         get_config.cache = get_config_real()
     return get_config.cache
 
 
-get_config.cache = ''
+get_config.cache = ""
 
 
 def dump(text, name):
     text = str(text)
-    with open(tmp_file_location + name, 'wb') as f:
+    with open(tmp_file_location + name, "wb") as f:
         f.write(bytes(text.encode()))
 
 
 def get_tree():
     tree = []
-    for (root, dirs, files) in os.walk(get_config()['dropbox_local_path'],
-            topdown=True, followlinks=True):
+    for (root, dirs, files) in os.walk(
+        get_config()["dropbox_local_path"], topdown=True, followlinks=True
+    ):
         for name in files:
             tree.append(os.path.join(root, name))
         for name in dirs:
             tree.append(os.path.join(root, name))
-    tree.sort(key = lambda s: -len(s)) #sort longest to smallest so that later files get deleted before the folders that they are in
+    tree.sort(
+        key=lambda s: -len(s)
+    )  # sort longest to smallest so that later files get deleted before the folders that they are in
     return tree
 
 
 def store_tree(tree):
-    tree = '\n'.join(tree)
-    dump(tree, 'drupebox_last_seen_files')
+    tree = "\n".join(tree)
+    dump(tree, "drupebox_last_seen_files")
 
 
 def load_tree():
     try:
-        last_tree = open(tmp_file_location + 'drupebox_last_seen_files', 'r').read()
+        last_tree = open(tmp_file_location + "drupebox_last_seen_files", "r").read()
     except:
-        last_tree = ''
-    last_tree = last_tree.split('\n')
+        last_tree = ""
+    last_tree = last_tree.split("\n")
 
     return last_tree
 
 
 def determine_deleted_files(tree_now, tree_last):
     deleted = []
-    if tree_last == ['']:
+    if tree_last == [""]:
         return []
     for element in tree_last:
         if not element in tree_now:
@@ -109,21 +124,29 @@ def determine_deleted_files(tree_now, tree_last):
 
 
 def upload(client, local_file_path, remote_file_path):
-    print('uuu', local_file_path)
-    f = open(local_file_path, 'rb')
-    client.files_upload(f.read(), remote_file_path, mute=True, mode=dropbox.files.WriteMode("overwrite",None))
+    print("uuu", local_file_path)
+    f = open(local_file_path, "rb")
+    client.files_upload(
+        f.read(),
+        remote_file_path,
+        mute=True,
+        mode=dropbox.files.WriteMode("overwrite", None),
+    )
+
 
 def download(client, remote_file_path, local_file_path):
-    print('ddd', remote_file_path)
+    print("ddd", remote_file_path)
     client.files_download_to_file(local_file_path, remote_file_path)
+
 
 def unix_time(timer):
     return time.mktime(timer.timetuple())
 
 
 def readable_time(timepoint):
-    return datetime.fromtimestamp(float(timepoint)).strftime('%a, %d %b %Y %H:%M:%S +0000'
-            )
+    return datetime.fromtimestamp(float(timepoint)).strftime(
+        "%a, %d %b %Y %H:%M:%S +0000"
+    )
 
 
 def path_exists(path):
@@ -137,58 +160,62 @@ def path_exists(path):
 def local_item_modified_time(local_file_path):
     return os.path.getmtime(local_file_path)
 
+
 def fp(path):
-	#Fix path function as dropbox root folder is "" not "/"
-	if path == "":
-		return path
-	if path == "/":
-		return ""
-	else:
-		if path[0] != "/":
-			return "/" + path
-		else:
-			return path
+    # Fix path function as dropbox root folder is "" not "/"
+    if path == "":
+        return path
+    if path == "/":
+        return ""
+    else:
+        if path[0] != "/":
+            return "/" + path
+        else:
+            return path
+
 
 def fix_local_time(client, remote_file_path):
     remote_path = remote_file_path
-    extra_path = '/'.join(remote_path.split('/')[0:-1])
-    info('fix local time on ' + remote_file_path)
+    extra_path = "/".join(remote_path.split("/")[0:-1])
+    info("fix local time on " + remote_file_path)
 
     tmp_folder = client.files_list_folder(fp(extra_path)).entries
     for tmp_item in tmp_folder:
         if tmp_item.path_display == remote_path:
             break  # found it
     tmp_time = tmp_item.client_modified
-    os.utime(get_config()['dropbox_local_path'] + remote_path,
-             (int(unix_time(tmp_time)), int(unix_time(tmp_time))))
+    os.utime(
+        get_config()["dropbox_local_path"] + remote_path,
+        (int(unix_time(tmp_time)), int(unix_time(tmp_time))),
+    )
 
 
 def skip(local_file_path):
-    local_item = local_file_path.split('/')[-1]
-    if local_item[0:len('.fuse_hidden')] == '.fuse_hidden':
-        fyi('ignore fuse hidden files')
+    local_item = local_file_path.split("/")[-1]
+    if local_item[0 : len(".fuse_hidden")] == ".fuse_hidden":
+        fyi("ignore fuse hidden files")
         return True
-    if local_item[-len('.pyc'):] == '.pyc':
-        fyi('ignore .pyc files')
+    if local_item[-len(".pyc") :] == ".pyc":
+        fyi("ignore .pyc files")
         return True
-    if local_item[-len('__pycache__'):] == '__pycache__':
-        fyi('ignore __pycache__')
+    if local_item[-len("__pycache__") :] == "__pycache__":
+        fyi("ignore __pycache__")
         return True
-    if local_item in ['.DS_Store', '._.DS_Store', 'DG1__DS_DIR_HDR', 'DG1__DS_VOL_HDR']:
-        fyi('ignore '+local_item)
+    if local_item in [".DS_Store", "._.DS_Store", "DG1__DS_DIR_HDR", "DG1__DS_VOL_HDR"]:
+        fyi("ignore " + local_item)
         return True
 
     try:
         local_time = local_item_modified_time(local_file_path)
     except:
-        print(('crash on local time check on', local_item))
+        print(("crash on local time check on", local_item))
         return True
     return False
 
 
 def local_item_not_found_at_remote(remote_folder, remote_file_path):
     remote_path = remote_file_path
-    extra_path = '/'.join(remote_path.split('/')[0:-1])
+    extra_path = "/".join(remote_path.split("/")[0:-1])
     remote_folder_path = extra_path
 
     unnaccounted_local_file = True
@@ -200,9 +227,11 @@ def local_item_not_found_at_remote(remote_folder, remote_file_path):
 
 def remote_item_modified_with_deleted(client, remote_file_path):
     remote_path = remote_file_path
-    extra_path = '/'.join(remote_path.split('/')[0:-1])
+    extra_path = "/".join(remote_path.split("/")[0:-1])
     remote_folder_path = extra_path
-    remote_folder_with_deleted = client.files_list_folder(fp(remote_folder_path),include_deleted=True).entries # client.metadata('/' + remote_folder_path, include_deleted=True)['contents']
+    remote_folder_with_deleted = client.files_list_folder(
+        fp(remote_folder_path), include_deleted=True
+    ).entries  # client.metadata('/' + remote_folder_path, include_deleted=True)['contents']
     folder_with_deleted = remote_folder_with_deleted
     remote_time = 0
     for unn_item in folder_with_deleted:
