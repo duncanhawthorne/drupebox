09a96d948d10404f854115531e753ce177218c12
Duncan Hawthorne <duncan.hawthorne@gmail.com>
2025-09-27 10:35:35 +0100

    tidy

--- config.py
+++ config.py
@@ -45,7 +45,7 @@ def _make_new_config_file(config_filename):
     user_path_tmp = add_trailing_slash(user_path_tmp)
     config_tmp["dropbox_local_path"] = user_path_tmp
 
-    os.makedirs(user_path_tmp, exist_ok=True)
+    os.makedirs(config_tmp["dropbox_local_path"], exist_ok=True)
     config_tmp["max_file_size"] = MAX_FILE_SIZE
     config_tmp["excluded_folder_paths"] = [
         "/home/pi/SUPER_SECRET_LOCATION_1/",
